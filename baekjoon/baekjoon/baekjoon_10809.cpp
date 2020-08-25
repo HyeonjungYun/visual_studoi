f070d4c1e47f8e4eee4a4db349e75aeb462b7663
yjn9971 <yjn5858@naver.com>
2020-08-25 16:36:05 +0900

    백준 10809 문자열에서 문자 찾기

---
+++ baekjoon/baekjoon/baekjoon_10809.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	string abc = "abcdefghijklmnopqrstuvwxyz";
+	cin >> s;
+
+	for (int i = 0; i < abc.length(); i++)
+	{
+		if (s.find(abc[i]) == string::npos)
+		{
+			cout << -1 << " ";
+		}
+		else
+			cout << s.find(abc[i]) << " ";
+		continue;
+	}
+
+	return 0;
+}
